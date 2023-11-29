90128627742dff2e7acd07a31e1f3598bb6fa1f5
neddington <neddington@cascadia.edu>
2023-11-29 14:53:28 -0800

    Initial Commit

---
+++ In Class Playing Audio and Video/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In Class Playing Audio and Video
+//
+//  Created by Eddington, Nick on 11/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In Class Playing Audio and Video/In_Class_Playing_Audio_and_VideoApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Playing_Audio_and_VideoApp.swift
+//  In Class Playing Audio and Video
+//
+//  Created by Eddington, Nick on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Playing_Audio_and_VideoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
